854935b2a62cb2b8e7138f0311e51d68b3fd71c4
Khrystyna Landvytovych <crofty.land@gmail.com>
2024-06-17 07:44:36 +0300

    feat: test config

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,7 +2,7 @@ import { useQuery, gql } from "@apollo/client";
 import React from "react";
 
 export const App = () => {
-  const GET_ISSUES = gql`
+	const GET_ISSUES = gql`
     query {
       repository(owner: "twolipp", name: "podcast") {
         # {
@@ -30,26 +30,26 @@ export const App = () => {
     }
   `;
 
-  // const get_ids = gql`
-  //   query {
-  //     hello
-  //   }
-  // `;
-  const { loading, error, data } = useQuery(GET_ISSUES);
+	// const get_ids = gql`
+	//   query {
+	//     hello
+	//   }
+	// `;
+	const { loading, error, data } = useQuery(GET_ISSUES);
 
-  // const { loading, error, data } = useQuery(get_ids);
+	// const { loading, error, data } = useQuery(get_ids);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error : {error.message}</p>;
 
-  return data.repository.issues.edges.map((item) => (
-    <div key={item.node.id}>
-      <h3>{item.node.title}</h3>
+	return data.repository.issues.edges.map(item => (
+		<div key={item.node.id}>
+			<h3>{item.node.title}</h3>
 
-      <br />
-      <b>About this location:</b>
-      <a href={`${item.node.url}`}>{item.node.url}</a>
-      <br />
-    </div>
-  ));
+			<br />
+			<b>About this location:</b>
+			<a href={`${item.node.url}`}>{item.node.url}</a>
+			<br />
+		</div>
+	));
 };
